ec9fbb7b02dca6e3ed582d44c3a1860b255af83a
Christina O'Donnell <cdo@mutix.org>
2024-02-03 08:47:49 +0000

    gitleaks: Add go import output
    Change-Id: I8e8b1752a9354300bc9c11eca04331a88ad4fd94

---
+++ go-import.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#guix import go github.com/rs/xid
+#guix import go github.com/coreos/go-systemd/v22
+#guix import go github.com/h2non/filetype
+#guix import go github.com/gitleaks/go-gitdiff
+#guix import go github.com/fatih/semgroup
+#guix import go github.com/charmbracelet/lipgloss
